2961f8b85e8a3cd03ebe619b371b3d8192897fe4
gueessa <gueessa@gmail.com>
2024-09-12 18:02:18 +0300

    fixes

--- src/app/core/shared/shared.definition.ts
+++ src/app/core/shared/shared.definition.ts
@@ -1,16 +1,13 @@
-import {OutsideClickDirective} from './directives/outside-click.directive';
 import {DraggingDirective} from './directives/dragging.directive';
 import {AssetsImagePipe} from './pipe/assets-image.pipe';
 
 export class SharedDefinition {
   static declarations = [
-    OutsideClickDirective,
     DraggingDirective,
     AssetsImagePipe,
   ];
 
   static exports = [
-    OutsideClickDirective,
     DraggingDirective,
     AssetsImagePipe,
   ];
